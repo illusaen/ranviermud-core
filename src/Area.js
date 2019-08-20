965cd83c7f3febf9262699cdc411c6cc6f622036
Shawn Biddle <shawncplus@gmail.com>
2019-08-19 20:37:13 -0400

    Add area events for npc add/remove

--- src/Area.js
+++ src/Area.js
@@ -132,6 +132,7 @@ class Area extends GameEntity {
    */
   addNpc(npc) {
     this.npcs.add(npc);
+    this.emit('npcAdded', npc);
   }
 
   /**
@@ -139,6 +140,7 @@ class Area extends GameEntity {
    * @param {Npc} npc
    */
   removeNpc(npc) {
+    this.emit('npcRemoved', npc);
     this.npcs.delete(npc);
   }
 
